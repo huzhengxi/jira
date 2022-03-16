f265e58d2caec07310b68d980f10c4d0533b1188
虎正玺 <15264316179@163.com>
2022-03-17 01:40:20 +0800

    feat: 增加错误边界处理

---
+++ src/components/error-boundary.tsx
@@ -0,0 +1,25 @@
+/**
+ * Created by jason on 2022/3/17.
+ */
+import React, {ReactNode} from 'react';
+
+type FallbackRender = (props: { error: Error | null }) => ReactNode
+
+export class ErrorBoundary extends React.Component<React.PropsWithChildren<{ fallbackRender: FallbackRender }>, { error: Error | null }> {
+  state = {
+    error: null
+  };
+
+  static getDerivedStateFromError(error: Error) {
+    return {error};
+  }
+
+  render() {
+    const {error} = this.state;
+    const {fallbackRender, children} = this.props;
+    if (error) {
+      return fallbackRender({error});
+    }
+    return children;
+  }
+}
